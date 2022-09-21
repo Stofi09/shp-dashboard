f8bb275bc9313b1d0a660c8e1f023f6e042c9dc7
Kris <kristof.solak@gmail.com>
2022-09-21 19:16:32 +0100

    first commit

---
+++ dev-stofi/src/main/java/com/devstofi/ShpDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.devstofi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShpDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShpDashboardApplication.class, args);
+	}
+
+}
---
+++ dev-stofi/src/test/java/com/devstofi/ShpDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstofi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShpDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
