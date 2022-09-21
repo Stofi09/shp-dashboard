6e0529651b1e1fb587c2a6ad3b4a3a2caf962d0b
Kris <kristof.solak@gmail.com>
2022-09-21 19:35:26 +0100

    Item added.

---
+++ dev-stofi/src/main/java/com/devstofi/dto/Item.java
@@ -0,0 +1,41 @@
+package com.devstofi.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private int quantity;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
