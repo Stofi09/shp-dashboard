c01e9726e19f64380dc022329f3bb4beccb626ec
Kris <kristof.solak@gmail.com>
2022-10-02 11:18:19 +0100

    changed structure

--- src/main/java/com/devstofi/controller/AuthController.java
+++ src/main/java/com/devstofi/controller/AuthController.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class AuthController {
 
